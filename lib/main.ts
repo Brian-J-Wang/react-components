2a528411a559e081a4af31304e977f6de00c02de
Brian Wang <brianwang7102@gmail.com>
2025-04-02 11:45:42 -0400

    fix: update directory name of component in lib/main.ts

--- lib/main.ts
+++ lib/main.ts
@@ -1,2 +1,2 @@
 export * from "./RichInput";
-export * from "./ResizbleInput"
+export * from "./ResizbleInput";
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({ include: ['lib'] })
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
       formats: ['es'],
     },
-    copyPublicDir: false
+    copyPublicDir: false,
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime'],
+    }
   }
 })
